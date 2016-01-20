e24a4173a718078c013fd98646e0e248cba9b067
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2016-01-20 16:37:40 +0700

    3. "Posting Data for Login and Upvote": "Saving Upvoted Stories"

--- DNApp/LocalDefaults.swift
+++ DNApp/LocalDefaults.swift
@@ -11,6 +11,8 @@ import UIKit
 struct LocalDefaults {
     static let userDefaults = NSUserDefaults.standardUserDefaults()
     
+    // MARK: - Token-related Methods
+    
     static func saveToken(token: String) {
         userDefaults.setObject(token, forKey: "tokenKey")
     }
@@ -21,5 +23,37 @@ struct LocalDefaults {
     
     static func deleteToken() {
         userDefaults.removeObjectForKey("tokenKey")
+    }
+    
+    // MARK: - Upvoted-related Methods
+    
+    static func saveUpvotedStory(storyID: Int) {
+        self.appendID(storyID, toKey: "upvotedStoriesKey")
+    }
+    
+    static func saveUpvotedComment(commentID: Int) {
+        self.appendID(commentID, toKey: "upvotedCommentsKey")
+    }
+    
+    static func isStoryUpvoted(storyID: Int) -> Bool {
+        return self.arrayForKey("upvotedStoriesKey", containsID: storyID)
+    }
+    
+    static func isCommentUpvoted(commentID: Int) -> Bool {
+        return self.arrayForKey("upvotedCommentsKey", containsID: commentID)
+    }
+    
+    // MARK - Helper Methods
+    
+    private static func arrayForKey(key: String, containsID id: Int) -> Bool {
+        let elements = userDefaults.arrayForKey(key) as? [Int] ?? []
+        return elements.contains(id)
+    }
+    
+    private static func appendID(id: Int, toKey key: String) {
+        let elements = userDefaults.arrayForKey(key) as? [Int] ?? []
+        if !elements.contains(id) {
+            userDefaults.setObject(elements + [id], forKey: key)
+        }
     }
 }
